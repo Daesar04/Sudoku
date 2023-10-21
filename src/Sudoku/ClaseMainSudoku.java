2a974eb28994febad642140674883a2d0e78178f
Mine-J <jsaezg@alumnos.nebrija.es>
2023-10-21 13:16:35 +0200

    comit

--- src/Sudoku/ClaseMainSudoku.java
+++ src/Sudoku/ClaseMainSudoku.java
@@ -1,4 +1,6 @@
 package Sudoku;
+import java.awt.EventQueue;
+
 import javax.swing.*;
 
 /**
@@ -12,11 +14,21 @@ public class ClaseMainSudoku
 	{
 		//Ventana_Sudoku v1 = new Ventana_Sudoku();
 		//v1.setVisible(true);
-		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					interfaz frame = new interfaz();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		/*
 		Juego_Sudoku juego = new Juego_Sudoku();
 		juego.generarSudoku();
 		SwingUtilities.invokeLater(() -> {
 			Ventana_Sudoku sudokuGUI = new Ventana_Sudoku(); // Inicializar la interfaz gráfica
-		});
+		});*/
 	}
 }
